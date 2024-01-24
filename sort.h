2db77ecada4fce539842e48c4b7b6fab1be07f3f
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-01-23 19:05:28 -0800

    betty head file

--- sort.h
+++ sort.h
@@ -16,9 +16,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
@@ -28,5 +28,5 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
-void quicksort (int *array, int low, int high, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
 #endif
